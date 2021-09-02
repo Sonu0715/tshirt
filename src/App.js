21041344659079f09dced462cd3a3811d6a6adcd
Sonu1507 <rsonu150794@gmail.com>
2021-09-02 20:24:12 +0530

    tshirst app full

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Dashboard from './components/dashboard/Dashboard';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Dashboard />
+    
+    )
+  }
+}
+
+export default App;
